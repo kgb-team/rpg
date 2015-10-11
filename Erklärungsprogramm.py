dcb8a5fe3975a92ea82a8a82737827162a78afce
hhhnchen <hhhchen@gmail.com>
2015-10-11 14:25:11 +0200

    change (non significant)

--- Erklärungsprogramm.py
+++ Erklärungsprogramm.py
@@ -5,16 +5,11 @@ def Jonas():
   y=2
   print(x, y)
 
-Jonas()
-Jonas()
-
-
 
 def Chris():
-  x="Chinokke"
-  y="Kartenmann"
-  look=x
-  use=y
+  
+  race="Chinokke"
+  use="Kartenmann"
   print("Christian ist ein", look, "dessen Nutzen es ist unser", use, "zu sein")
 
 Chris()
